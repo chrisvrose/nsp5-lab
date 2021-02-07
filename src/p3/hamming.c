49321cea8abfb149d130c4c7986d1c5aae615291
Atreya Bain <christophervrose@gmail.com>
2021-02-07 19:01:05 +0530

    why

--- src/p3/hamming.c
+++ src/p3/hamming.c
@@ -19,11 +19,11 @@ int main() {
  
     //Calculation of even parity
     data[6]=data[0]^data[2]^data[4];
-	data[5]=data[0]^data[1]^data[4];
-	data[3]=data[0]^data[1]^data[2];
+    data[5]=data[0]^data[1]^data[4];
+    data[3]=data[0]^data[1]^data[2];
  
-	printf("\nEncoded data is\n");
-	for(i=0;i<7;i++)
+    printf("\nEncoded data is\n");
+    for(i=0;i<7;i++)
         printf("%d",data[i]);
  
     printf("\n\nEnter received data bits one by one\n");
@@ -31,36 +31,36 @@ int main() {
         scanf("%d",&dataatrec[i]);
  
     c1=dataatrec[6]^dataatrec[4]^dataatrec[2]^dataatrec[0];
-	c2=dataatrec[5]^dataatrec[4]^dataatrec[1]^dataatrec[0];
-	c3=dataatrec[3]^dataatrec[2]^dataatrec[1]^dataatrec[0];
-	c=c3*4+c2*2+c1 ;
+    c2=dataatrec[5]^dataatrec[4]^dataatrec[1]^dataatrec[0];
+    c3=dataatrec[3]^dataatrec[2]^dataatrec[1]^dataatrec[0];
+    c=c3*4+c2*2+c1 ;
  
     if(c==0) {
-		printf("\nNo error while transmission of data\n");
+        printf("\nNo error while transmission of data\n");
     }
-	else {
-		printf("\nError on position %d",c);
-    	
-		printf("\nData sent : ");
-        for(i=0;i<7;i++)
-        	printf("%d",data[i]);
+    else {
+        printf("\nError on position %d",c);
         
-		printf("\nData received\n: ");
+        printf("\nData sent : ");
         for(i=0;i<7;i++)
-        	printf("%d",dataatrec[i]);
-		
-		printf("\nCorrect message is\n");
+            printf("%d",data[i]);
+        
+        printf("\nData received\n: ");
+        for(i=0;i<7;i++)
+            printf("%d",dataatrec[i]);
+        
+        printf("\nCorrect message is\n");
  
-		//if errorneous bit is 0 we complement it else vice versa
-		if(dataatrec[7-c]==0)
-			dataatrec[7-c]=1;
+        //if errorneous bit is 0 we complement it else vice versa
+        if(dataatrec[7-c]==0)
+            dataatrec[7-c]=1;
         else
-			dataatrec[7-c]=0;
-		
-		for (i=0;i<7;i++) {
-			printf("%d",dataatrec[i]);
-		}
-	}
+            dataatrec[7-c]=0;
+        
+        for (i=0;i<7;i++) {
+            printf("%d",dataatrec[i]);
+        }
+    }
     printf("\n");
     return 0;
 }
